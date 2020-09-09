ef34a496e99583d2cada8e4a659de9db3855f870
wangqiucheng <wangqiucheng@bigo.sg>
2020-09-09 11:00:54 +0800

    dfs && bfs

---
+++ back_trace/77.h
@@ -0,0 +1,27 @@
+//
+// Created by wangqiucheng on 2020/9/8.
+//
+
+#ifndef LEETCODE_TEST_77_H
+#define LEETCODE_TEST_77_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void back_trace(int n, int k, vector<vector<int>>& vv) {
+
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> vv;
+        if (n < k) {
+            return vv;
+        }
+
+    }
+};
+
+#endif //LEETCODE_TEST_77_H
--- graph/133.h
+++ graph/133.h
@@ -6,16 +6,36 @@
 #define LEETCODE_TEST_133_H
 
 #include "common.h"
-#include <stack>
-#include <unordered_set>
+#include <map>
+
+// 图遍历, dfs + bfs
 
 class Solution {
 public:
+    std::map<Node*, Node*> visited;
+
+    // dfs
     Node* cloneGraph(Node* node) {
         if (!node) {
             return nullptr;
         }
+        if (visited.find(node) != visited.end()) {
+            return visited[node];
+        }
+        Node* clone_node = new Node(node->val);
+        visited[node] = clone_node;
+
+        for (auto& neighbour : node->neighbors) {
+            clone_node->neighbors.emplace_back(cloneGraph(neighbour));
+        }
+
+        return clone_node;
+    }
+
+    Node* cloneGraphV2(Node* node) {
+
     }
 };
+
 
 #endif //LEETCODE_TEST_133_H
