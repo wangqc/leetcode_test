9613d989305844af8980862bb6b2cf99bea16586
wangqiucheng <wangqiucheng@bigo.sg>
2020-09-10 18:24:38 +0800

    并查集是个新知识点

--- search/200.h
+++ search/200.h
@@ -17,17 +17,17 @@ public:
         int c_size = grid[0].size();
 
         grid[r][c] = '0';
-        if (r - 1 >= 0 && grid[r-1][c] == '1') {
-            dfs(grid, r-1, c);
+        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+            dfs(grid, r - 1, c);
         }
-        if (r + 1 < r_size && grid[r+1][c] == '1') {
-            dfs(grid, r+1, c);
+        if (r + 1 < r_size && grid[r + 1][c] == '1') {
+            dfs(grid, r + 1, c);
         }
-        if (c-1 >= 0 && grid[r][c-1] == '1') {
-            dfs(grid, r, c-1);
+        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+            dfs(grid, r, c - 1);
         }
-        if (c+1 < c_size && grid[r][c+1] == '1') {
-            dfs(grid, r, c+1);
+        if (c + 1 < c_size && grid[r][c + 1] == '1') {
+            dfs(grid, r, c + 1);
         }
     }
 
@@ -42,7 +42,7 @@ public:
         for (int i = 0; i < i_size; ++i) {
             for (int j = 0; j < j_size; ++j) {
                 if (grid[i][j] == '1') {
-                    count ++;
+                    count++;
                     dfs(grid, i, j);
                 }
             }
@@ -65,19 +65,19 @@ public:
                 int col = pr.second;
                 pos_que.pop();
                 if (row - 1 >= 0 && grid[row - 1][col] == '1') {
-                    grid[row-1][col] = '0';
+                    grid[row - 1][col] = '0';
                     pos_que.push(make_pair(row - 1, col));
                 }
                 if (row + 1 < r_size && grid[row + 1][col] == '1') {
-                    grid[row+1][col] = '0';
+                    grid[row + 1][col] = '0';
                     pos_que.push(make_pair(row + 1, col));
                 }
                 if (col - 1 >= 0 && grid[row][col - 1] == '1') {
-                    grid[row][col-1] = '0';
-                    pos_que.push(make_pair(row, col-1));
+                    grid[row][col - 1] = '0';
+                    pos_que.push(make_pair(row, col - 1));
                 }
                 if (col + 1 < c_size && grid[row][col + 1] == '1') {
-                    grid[row][col+1] = '0';
+                    grid[row][col + 1] = '0';
                     pos_que.push(make_pair(row, col + 1));
                 }
             }
@@ -102,6 +102,85 @@ public:
         }
         return count;
     }
+
+    class UnionFind {
+    public:
+        UnionFind(vector<vector<char>>& grid) {
+            count = 0;
+            int r_size = grid.size();
+            int c_size = grid[0].size();
+
+            for (int r = 0; r < r_size; ++r) {
+                for (int c = 0; c < c_size; ++c) {
+                    int idx = r * c_size + c;
+                    if (grid[r][c] == '1') {
+                        parents.push_back(idx);
+                        count++;
+                    } else {
+                        parents.push_back(-1);
+                    }
+                    rank.push_back(0);
+                }
+            }
+        }
+
+        // only when grid[r][c] is '1' to call, so parents[x] not -1
+        int find(int x) {
+            if (parents[x] != x) {
+                parents[x] = find(parents[x]);
+            }
+            return parents[x];
+        }
+
+        void connect(int x, int y) {
+            int root_x = find(x);
+            int root_y = find(y);
+            if (root_x != root_y) {
+                if (rank[root_x] < rank[root_y]) {
+                    swap(root_x, root_y);
+                }
+                parents[root_y] = root_x;
+                if (rank[root_x] == rank[root_y]) {
+                    rank[root_x]++;
+                }
+                count--;
+            }
+        }
+
+        int get_count() {
+            return count;
+        }
+
+    private:
+        vector<int> parents;
+        vector<int> rank;
+        int count;
+    };
+
+    int numIslandsV3(vector<vector<char>>& grid) {
+        int r_size = grid.size();
+        if (r_size == 0) {
+            return 0;
+        }
+        int c_size = grid[0].size();
+
+        UnionFind unionFind(grid);
+        for (int r = 0; r < r_size; ++r) {
+            for (int c = 0; c < c_size; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] == '0';
+                    if (r + 1 < r_size && grid[r + 1][c] == '1') {
+                        unionFind.connect(r * c_size + c, (r + 1) * c_size + c);
+                    }
+                    if (c + 1 < c_size && grid[r][c + 1] == '1') {
+                        unionFind.connect(r * c_size + c, r * c_size + c + 1);
+                    }
+                }
+            }
+        }
+        return unionFind.get_count();
+    }
+
 };
 
 #endif //LEETCODE_TEST_200_H
