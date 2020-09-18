1c92e539f0a51fec0c517848632885748325dafe
wangqiucheng <wangqiucheng@bigo.sg>
2020-09-18 23:08:42 +0800

    回溯搜索

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include "queue/542.h"
+#include "back_trace/47.h"
 
 using namespace std;
 
 int main() {
     Solution s;
-    vector<vector<int>> matrics{
-        {0, 0, 0},
-        {0, 1, 0},
-        {1, 1, 1}};
-    vector<vector<int>> ret = s.updateMatrixV2(matrics);
+    vector<int> nums{1,32,3,2,1,3,3,1,23,4};
+    vector<vector<int>> ret = s.permuteUnique(nums);
     for (auto& v : ret) {
         for (auto& e : v) {
             cout << e << ",";
