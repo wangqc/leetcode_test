c15f203ee21deee36ec31ee54fb59ceba8a41d0e
wangqiucheng <wangqiucheng@bigo.sg>
2020-10-13 20:16:51 +0800

    linklist and tree

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include "dp/45.h"
+#include "linklist/24.h"
 
 using namespace std;
 
 int main() {
     Solution s;
-    vector<int> v{7};
-    cout << s.jump(v) << endl;
+    ListNode* head = new ListNode(1);
+    ListNode* cur = head;
+    for (int i = 2; i < 11; ++i) {
+        cur->next = new ListNode(i);
+        cur = cur->next;
+    }
+    cur = head;
+    while (cur) {
+        cout << cur->val << ",";
+        cur = cur->next;
+    }
+    cout << endl;
+    ListNode* swap_head = s.swapPairsV3(head);
+    while (swap_head) {
+        cout << swap_head->val << ",";
+        swap_head = swap_head->next;
+    }
+    cout << endl;
     return 0;
 }
