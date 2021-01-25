9e77493e49e0bd516f7d25513dd356276396292c
wangqc <applefall@163.com>
2021-01-25 15:15:22 +0800

    相交链表解法2种

--- linklist/160.h
+++ linklist/160.h
@@ -9,8 +9,27 @@
 
 class Solution {
 public:
+    // https://leetcode-cn.com/problems/intersection-of-two-linked-lists/submissions/
+    // 最简单想到的是哈希法。 不过可以考虑重走对方走过的路来找到相交点
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-
+        ListNode* cur_a = headA;
+        ListNode* cur_b = headB;
+        while (1) {
+            if (cur_a == cur_b) {
+                return cur_a;
+            }
+            if (!cur_a) {
+                cur_a = headB;
+            } else {
+                cur_a = cur_a->next;
+            }
+            if (!cur_b) {
+                cur_b = headA;
+            } else {
+                cur_b = cur_b->next;
+            }
+        }
+        return nullptr;
     }
 
 private:
